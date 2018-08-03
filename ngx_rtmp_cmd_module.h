d103fe459051bcaf5e9f2c834cca9817eb105e3b
b073r <bobrowski_michal@yahoo.com>
2018-08-03 14:53:18 +0200

    Max url 1536 bytes

--- ngx_rtmp_cmd_module.h
+++ ngx_rtmp_cmd_module.h
@@ -14,9 +14,9 @@
 #include "ngx_rtmp.h"
 
 
-#define NGX_RTMP_MAX_NAME           256
+#define NGX_RTMP_MAX_NAME           1536
 #define NGX_RTMP_MAX_URL            1536
-#define NGX_RTMP_MAX_ARGS           1280
+#define NGX_RTMP_MAX_ARGS           1536
 
 
 /* Basic RTMP call support */
